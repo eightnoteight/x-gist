e2eacc73bf2aba3127a5cdc5c779c120d5867874
eightnoteight <mr.eightnoteight@gmail.com>
2014-08-09 18:45:48 +0530

    ahh never mind

--- gist/x_gist.py
+++ gist/x_gist.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+
+import argparse
+import requests
+
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-l'
+						'--login',
+						dest='login',
+						default=False,
+						action='store_true',
+						help="login with github account")
+	parser.add_argument('')
+def main():
+	'''
+	'''
+	args = parse_args()
+if __name__ == '__main__':
+	main()
